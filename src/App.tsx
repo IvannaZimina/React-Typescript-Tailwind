ddb91ed8e97e7b9c84e5b5cf34fab57808da3d8c
Zimina Ivanna <ivanna.zimina@plus4u.net>
2022-09-10 16:35:46 +0300

    single-page app created

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { Product } from "./components/product"
+import { useProducts } from "./hooks/products"
+
+function App() {
+  const { loading, error, products } = useProducts()
+
+  return (
+    <div className="container mx-auto max-w-2xl pt-5">
+      {loading && <p className="text-center">Loading...</p>}
+      {error && <p className="text-center text-red-600">{error}</p>}
+      {products.map((product) => (
+        <Product product={product} key={product.id} />
+      ))}
+    </div>
+  )
+}
+
+export default App
---
+++ src/hooks/products.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosError } from "axios"
+import { useEffect, useState } from "react"
+import { IProduct } from "../models"
+
+export function useProducts() {
+  const [products, setProduct] = useState<IProduct[]>([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
+
+  async function fetchProducts() {
+    try {
+      setError("")
+      setLoading(true)
+      const response = await axios.get<IProduct[]>(
+        `https://fakestoreapi.com/products/?limit=5`
+      )
+      setProduct(response.data)
+      setLoading(false)
+    } catch (err: unknown) {
+      const error = err as AxiosError
+      setLoading(false)
+      setError(error.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+  return { loading, error, products }
+}
